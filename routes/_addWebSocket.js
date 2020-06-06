01f550c53330dec5f3ca7ec36be9f3d36850596a
Widi Harsojo <wharsojo@gmail.com>
2020-06-06 10:22:28 +0800

    add pseudo code

--- routes/_addWebSocket.js
+++ routes/_addWebSocket.js
@@ -1,9 +1,16 @@
 const { script_src } = require('./fetch');
 
+const headerchg = headers => {
+  if (headers['some-header-key']) {
+    //action...
+  }
+}
+
 function addWebSocket(arr, reqs) {
   if ((reqs.headers.accept+'').indexOf('text/html') > -1) {
     arr.push(resp => { 
       resp.body = script_src(resp.body, ['websocket.js']);
+      headerchg(resp.headers);
       return resp;
     });
   }
