ea772e098deedaaec84dac324d56505be54854b7
Widi Harsojo <wharsojo@gmail.com>
2020-06-11 22:22:43 +0800

    Remove unnecessary logic

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const c = require('ansi-colors');
-const _chgRequest = require('./_chgRequest');
+const {extract, fetch} = require('./fetch');
 const _jsResponse = require('./_jsResponse');
+const _chngRequest = require('./_chngRequest');
 const _cssResponse = require('./_cssResponse');
 const _logResponse = require('./_logResponse');
 const _jsonResponse = require('./_jsonResponse');
@@ -10,22 +11,21 @@ const _skipResponse = require('./_skipResponse');
 const _mockResponse = require('./_mockResponse');
 const _addWebSocket = require('./_addWebSocket');
 const _cacheResponse = require('./_cacheResponse');
-const patchReqHeader = require('./patchReqHeader');
-const { extract, fetch } = require('./fetch');
+
+const _resp = {
+  status: 200,
+  headers: {},
+  body: ''
+};
 
 module.exports =  (route, request) => {
   const reqs = extract(route, request);
+  const {nosocket, proxy} = mitm.argv;
   const respEvents = [];
-  const {argv} = mitm;
 
   // catch unknown url scheme & respond it 
   if (reqs.url.match('(brave|edge)://')) {
-    const resp = {
-      status: 200,
-      headers: {},
-      body: ''
-    };
-    route.fulfill(resp);
+    route.fulfill(_resp);
     return;
   }
 
@@ -36,17 +36,13 @@ module.exports =  (route, request) => {
     return;
   }
 
-  if (argv.proxy) {
-    if (_proxyRequest(reqs)) {
-      reqs.proxy = argv.proxy;
-    }
-  }
-
   if (_mockResponse(route, reqs)) {
     return;
   }
 
-  _chgRequest(reqs);
+  if (proxy && _proxyRequest(reqs)) {
+    reqs.proxy = proxy;
+  }
 
   //--resp can be undefined or local cached & can skip logs (.nolog)
   let {match, resp} = _cacheResponse(respEvents, reqs);
@@ -61,35 +57,25 @@ module.exports =  (route, request) => {
   _cssResponse(respEvents, reqs);
   _jsResponse(respEvents, reqs);
 
-  if (!argv.nosocket) {
+  if (!nosocket) {
     //--inject websocket client to html
     _addWebSocket(respEvents, reqs);
   }
 
-  //--respond need to log or modify
-  if (respEvents.length) {
-    if (resp) {
-      //--log or respond with cached
+  if (resp) {
+    //--log or respond with cached
+    for (const fn of respEvents) {
+      resp = fn(resp);
+    }
+    route.fulfill(resp); // exec route.fulfill()
+  } else {
+    //--call to BE and do log or modification & respond
+    _chngRequest(reqs);
+    fetch(route, reqs, function(resp) {
       for (const fn of respEvents) {
         resp = fn(resp);
       }
-      route.fulfill(resp); // exec route.fulfill()
-    } else {
-      //--call to BE and do log or modification & respond
-      fetch(route, reqs, function(resp) {
-        for (const fn of respEvents) {
-          resp = fn(resp);
-        }
-        return resp; // exec route.fulfill()
-      });
-    }
-  } else {
-    if (resp) {
-      //--respond with cached
-      route.fulfill(resp); // exec route.fulfill()
-    } else {
-      //--normal flow: with reg headers getting update
-      patchReqHeader(route, reqs); // exec route.continue();
-    }
+      return resp; // exec route.fulfill()
+    });
   }
 }
