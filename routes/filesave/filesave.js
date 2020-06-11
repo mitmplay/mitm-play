7563821f71e68c72552fb1dbb6525946c3ad65ac
Widi Harsojo <wharsojo@gmail.com>
2020-06-11 08:53:14 +0800

    Fix cache binary image

--- routes/filesave/filesave.js
+++ routes/filesave/filesave.js
@@ -3,7 +3,7 @@ const c = require('ansi-colors');
 
 module.exports = ({fpath1, body}, {fpath2, meta}, typ) => {
   fs.ensureFile(fpath1, err => {
-    fs.writeFile(fpath1, body , err => {
+    fs.writeFile(fpath1, body, err => {
       err && console.log(c.redBright(`>> Error write body ${typ}`), err);
     })
   })
