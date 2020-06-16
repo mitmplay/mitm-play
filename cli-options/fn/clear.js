9c4c418458e6577fcc5d4699b878170bb3230526
Widi Harsojo <wharsojo@gmail.com>
2020-06-16 14:07:44 +0800

    add file event handler

--- cli-options/fn/clear.js
+++ cli-options/fn/clear.js
@@ -1,13 +1,19 @@
 const fs = require('fs-extra');
 
+function ehandler(err) {
+  if (err) {
+    console.log('Error remove files', err);
+  }
+}
+
 function clear() {
   const {browser,delete: d} = global.mitm.argv;
   for (let browserName in browser) {
     if (d===true) {
-      fs.remove(`${global.mitm.home}/${browserName}/cache`);
-      fs.remove(`${global.mitm.home}/${browserName}/log`);     
+      fs.remove(`${global.mitm.home}/${browserName}/cache`, ehandler);
+      fs.remove(`${global.mitm.home}/${browserName}/log`,   ehandler);
     } else {
-      fs.remove(`${global.mitm.home}/${browserName}/${d}`);
+      fs.remove(`${global.mitm.home}/${browserName}/${d}`,  ehandler);
     }
   }
 }
