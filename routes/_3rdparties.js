c4746794802bcc6a531540cb0a0f01aa39108233
Widi Harsojo <wharsojo@gmail.com>
2020-06-27 09:26:01 +0800

    Update code - remove overhead

--- routes/_3rdparties.js
+++ routes/_3rdparties.js
@@ -1,20 +1,16 @@
 const c = require('ansi-colors');
 const nameSpace = require('./namespace');
+const {spliter,fn: {tldomain}} = global.mitm;
 
 function thirdparty({url, headers}) {
   const {origin, referer} = headers;
-  const {
-    spliter,
-    fn: {tldomain}
-  } = global.mitm;
-
-  let domain = tldomain(url);
+  const domain = tldomain(url);
 
   if (!nameSpace(domain)) {
-    if (origin && nameSpace(domain)) return;
+    if (origin && nameSpace(origin)) return;
     if (referer && nameSpace(referer)) return;
     console.log(c.redBright(`>> no-namespace (${url.split(spliter)[0]})`));
-    return {namespace: domain};
+    return true;
   }
 }
 
