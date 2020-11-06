6473bfbe0b6daece6623e254b03a4522939a0696
Widi Harsojo <wharsojo@gmail.com>
2020-11-06 22:12:58 +0800

    feat: open logs folder

--- plugins/chrome/src/components/logs/Button2.svelte
+++ plugins/chrome/src/components/logs/Button2.svelte
@@ -1,5 +1,6 @@
 <script>
 import { tabstore } from './tab.js';
+import { logstore } from './stores.js';
 
 function btnMin() {
   const {tab, editor} = $tabstore;
@@ -12,11 +13,22 @@ function btnPlus() {
   const id = `editor${tab+1}`;
   editor[id].trigger('fold', 'editor.unfoldAll');
 }
+
+function btnOpen() {
+  let arr = $logstore.path.split('/')
+  arr.pop();
+  const path = arr.join('/');
+  console.log({path});
+  ws__send('openFolder', {path}, data => {
+    console.log('Done Open!');
+  });
+}
 </script>
 
 <div class="btn-container">
   <button class="tlb btn-min"  on:click="{btnMin}" >[--]</button> -
-  <button class="tlb btn-plus" on:click="{btnPlus}">[++]</button>.
+  <button class="tlb btn-plus" on:click="{btnPlus}">[++]</button> -
+  <button class="tlb btn-open" on:click="{btnOpen}">Open</button>
 </div>
 
 <style>
