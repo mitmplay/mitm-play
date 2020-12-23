e4fe0de2afcd6bb6edf429c734006e896aa3e73f
wharsojo <wharsojo@gmail.com>
2020-12-23 15:45:38 +0800

    feat: make it compatible with fish shell

--- bin/mitm-play.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node --max-http-header-size=40960
-
-require('../mitm-play')
--- mitm-play.js
+++ mitm-play.js
@@ -1,5 +1,11 @@
 const c = require('ansi-colors')
 
+if (process.env.NODE_OPTIONS.match(/--max-http-header-size/)) {
+  console.log(c.red('Please check your NODE_OPTIONS: --max-http-header-size atleast 40960'))
+} else {
+  process.env.NODE_OPTIONS += ' --max-http-header-size=40960'
+}
+
 global.__app = __dirname.replace(/\\/g, '/')
 console.log(c.yellow(`>>> app ${global.__app}`))
 
