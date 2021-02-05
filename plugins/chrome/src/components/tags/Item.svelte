a23bbc2ee39e04583ecf25b64bfa30e3abbd3e9c
wharsojo <wharsojo@gmail.com>
2021-02-05 19:03:09 +0800

    tags: add list and resize-able content

---
+++ plugins/chrome/src/components/tags/Item.svelte
@@ -0,0 +1,30 @@
+<script> // feat: profile
+// import { source } from './stores.js';
+
+export let item;
+export let onChange;
+
+function clickHandler(e) {
+
+}
+</script>
+
+<div class="td-item {$source.fpath===item.fpath}"
+  data-item={item.element}
+  on:click="{clickHandler}"
+>{item.title}</div>
+
+<style>
+.td-item {
+  cursor: pointer;
+  padding: 0.1rem;
+  line-height: 15px;
+  padding-left: 5px;
+  border-bottom: 1px solid #c0d8cca1;
+}
+.td-item.true {
+  color: blue;
+  font-weight: bolder;
+  background: greenyellow;
+}
+</style>
---
+++ plugins/chrome/src/components/tags/List.svelte
@@ -0,0 +1,27 @@
+<script>
+export let onChange;
+
+import { onMount } from 'svelte';
+import Item from './Item.svelte';
+
+let rerender = 0;
+let data = [];
+
+$: _data = data;
+
+onMount(async () => {
+
+});
+
+const profileHandler = obj => {
+
+}
+
+// window.mitm.files.tags_events.profileTable = () => {
+
+// }
+</script>
+
+{#each Object.keys(_data) as item}
+  <Item item={{element: item, ..._data[item]}} {onChange}/>
+{/each}
