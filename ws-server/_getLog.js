f923d353b655625780ca48cb6179790fc5dab7d6
wharsojo <wharsojo@gmail.com>
2021-04-11 01:17:27 +0800

    feat: check if logs having incorrect file name

--- ws-server/_getLog.js
+++ ws-server/_getLog.js
@@ -1,4 +1,5 @@
 const { chromium, firefox } = require("playwright")
+const c = require('ansi-colors')
 
 const regex = /(chromium|firefox|webkit)\/log\/page~(\d+)-(session~(\d+)|undefined)/
 
@@ -10,23 +11,27 @@ module.exports = () => {
   const temp = {}
   logSorted.forEach(element => {
     const arr  = element.match(regex)
-    const id1 = arr.slice(2).join('-')
-    const brw = arr[1].padEnd(8, '_')
-    const grp1 = `${brw}~${id1}`
-    if (temp[grp1]===undefined) {
-      temp[grp1] = {browser}
-      browser[brw] += 1
-    }
-    const id2 = `${browser[brw]}`.padStart(2, '0')
-    const grp2 = `${brw}~${id2}`
-    if (data[grp2]===undefined) {
-      data[grp2] = {}
-    }
-    const [path, title] = element.split('@')
-    data[grp2][element] = {
-      ..._log[element],
-      title,
-      path
+    if (arr!==null) {
+      const id1 = arr.slice(2).join('-')
+      const brw = arr[1].padEnd(8, '_')
+      const grp1 = `${brw}~${id1}`
+      if (temp[grp1]===undefined) {
+        temp[grp1] = {browser}
+        browser[brw] += 1
+      }
+      const id2 = `${browser[brw]}`.padStart(2, '0')
+      const grp2 = `${brw}~${id2}`
+      if (data[grp2]===undefined) {
+        data[grp2] = {}
+      }
+      const [path, title] = element.split('@')
+      data[grp2][element] = {
+        ..._log[element],
+        title,
+        path
+      }  
+    } else {
+      console.log(c.red(`Error in log file ${element}`))
     }
   })
   return data
