06f8d3dd4608351587d4b110ee2058b0c4613e05
wharsojo <wharsojo@gmail.com>
2021-04-28 01:37:05 +0800

    refactor autofill

--- ws-server/_autofill.js
+++ ws-server/_autofill.js
@@ -12,18 +12,17 @@ module.exports = async ({ data }) => {
   for await (let obj of autofill) {
     if (typeof (obj) === 'string') {
       console.log(c.greenBright(`   ${obj}`))
-      if (obj.match(/^ *[=-]>/)) {
-        obj = `${lastObj.split(/^(.+)([=-]>)/)[1]}${obj}`
+      if (lastObj && obj.match(/^ *[=-]>/)) {
+        obj = `${lastObj.split(/^(.*)([=-]>)/)[1]}${obj}`
       }
       lastObj = obj
-      const [selector, typ, value] = obj.match(/^(.+)([=-]>)(.+)$/).slice(1).map(x => x.trim())
+      const [selector, typ, value] = obj.match(/^(.*)([=-]>)(.*)$/).slice(1).map(x => x.trim())
       if (typ === '=>') {
         obj = { selector, value }
       } else if (typ === '->') {
-        let arr = value.split('~>')
-        if (arr[1]) {
-          arr = arr.map(x => x.trim())
-          obj = { selector, action: arr[0], options: arr[1] }
+        const [action, options] = value.split('~>').map(x => x.trim())
+        if (options) {
+          obj = { selector, action, options }
         } else {
           obj = { selector, action: value }
         }
@@ -33,33 +32,52 @@ module.exports = async ({ data }) => {
     } else {
       console.log(c.greenBright(`${JSON.stringify(obj, null, 2)}`))
     }
-    if (obj.action) {
+    /**
+     * valid variations
+     * =========================
+     * input[name="firstname"]=>
+     * => how are you
+     * -> newpage ~> https://mailinator.com
+     * #addOverlay -> type ~> widi
+     * -> press ~> Enter
+     * #inbox_field -> wait
+     * -> save ~> getAttribute~class:mailinator
+     * -> close
+     * input[name="email"] => :mailinator
+     */
+    const {
+      selector,
+      action,
+      value,
+    } = obj
+
+    if (action) {
       const options = obj.options || {}
-      if (obj.action === 'type') {
-        await input('type', obj.selector, options)
-      } else if (obj.action === 'wait') {
-        await page.waitForSelector(obj.selector)
-      } else if (obj.action === 'press') {
-        await page.press(obj.selector, options)
-      } else if (obj.action === 'click') {
-        await page.click(obj.selector, options)
-      } else if (obj.action === 'focus') {
-        await page.focus(obj.selector)
-      } else if (obj.action === 'check') {
-        await page.check(obj.selector)
-      } else if (obj.action === 'uncheck') {
-        await page.uncheck(obj.selector)
-      } else if (obj.action === 'selectOption') {
-        await page.selectOption(obj.selector, options)
-      } else if (obj.action === 'newpage') {
+      if (action === 'type') {
+        await input('type', selector, options)
+      } else if (action === 'wait') {
+        await page.waitForSelector(selector)
+      } else if (action === 'press') {
+        await page.press(selector, options)
+      } else if (action === 'click') {
+        await page.click(selector, options)
+      } else if (action === 'focus') {
+        await page.focus(selector)
+      } else if (action === 'check') {
+        await page.check(selector)
+      } else if (action === 'uncheck') {
+        await page.uncheck(selector)
+      } else if (action === 'selectOption') {
+        await page.selectOption(selector, options)
+      } else if (action === 'newpage') {
         page = await browser.newPage()
         attach(page)
         await page.goto(options)
-      } else if (obj.action === 'close') {
+      } else if (action === 'close') {
         await page.close()
         page = oldPage
-      } else if (obj.action === 'save') {
-        const obj2 = await page.$eval(obj.selector, (e, opt) => {
+      } else if (action === 'save') {
+        const obj2 = await page.$eval(selector, (e, opt) => {
           const [attr, key] = opt.split(':')
           const [id1, id2] = attr.split('~')
           const value = id2 ? e[id1](id2) : e[id1]
@@ -70,8 +88,8 @@ module.exports = async ({ data }) => {
           localStorage.setItem(key, val)
         }, obj2);
       }
-    } else if (obj.value) {
-      await input('fill', obj.selector, obj.value)
+    } else if (value) {
+      await input('fill', selector, value)
     }
   }
   return { ok: 'OK' }
