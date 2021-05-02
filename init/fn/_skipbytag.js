faf2a28473fa9da634aba39732c03df207f89951
wharsojo <wharsojo@gmail.com>
2021-05-02 22:38:45 +0800

    fix: check on __tag2

--- init/fn/_skipbytag.js
+++ init/fn/_skipbytag.js
@@ -18,24 +18,31 @@ function skipTags(tags) {
   return true
 }
 
-function checkTags(namespace, key, typ, skip=false) {
+function checkTags(namespace, key, typ, tags, skip=false) {
   const {__tag1, __tag2, __tag3} = global.mitm
-  if (!__tag3[namespace] || !__tag3[namespace][key]) {
-    return skip
-  }
-  const tag3 = __tag3[namespace][key][typ]
-  if (tag3===undefined) {
-    return skip
-  }
-  let {tag1, tag2, tags} = tag3 // feat: update __tag3
-  if (tag1.length) {
-    skip = skipTag1(tag1,  __tag1[namespace])
-  }
-  if (!skip && tag2 &&  __tag2[namespace][tag2].state) { // feat: no need to check tag3
-    return false
-  }
-  if (!skip && tags.length) {
-    skip = skipTags(tags)
+  if (__tag3[namespace] && __tag3[namespace][key]) {
+    const tag3 = __tag3[namespace][key][typ]
+    if (tag3===undefined) {
+      return skip
+    }
+    let {tag1, tag2, tags} = tag3 // feat: update __tag3
+    if (tag1.length) {
+      skip = skipTag1(tag1,  __tag1[namespace])
+    }
+    if (!skip && tag2 &&  __tag2[namespace][tag2].state) { // feat: no need to check tag3
+      return false
+    }
+    if (!skip && tags.length) {
+      skip = skipTags(tags)
+    }
+  } else if (tags.match(':')) {
+    const [tag, ...tag1] = tags.split(/ +/)
+    const obj = __tag2[namespace][tag]
+    if (obj && !obj.state) {
+      return true
+    } else if (tag1.length) {
+      skip = skipTag1(tag1,  __tag1[namespace])
+    }
   }
   return skip
 }
@@ -49,9 +56,9 @@ function _skipByTag (match, typ) {
     const [, method,, path] = arrTag
     key = method ? `${method}:${path}` : path // remove from url
   }
-  let skip = checkTags(namespace, key, typ)
-  if (skip) {
-    skip = checkTags('_global_', key, typ, skip)
+  let skip = checkTags(namespace, key, typ, match.typ)
+  if (skip && namespace!=='_global_') {
+    skip = checkTags('_global_', key, typ, match.typ, skip)
   }
   if (skip) {
     if (__args.verbose) {
