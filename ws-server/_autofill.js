7d3da7c99f3987e103717cbce37abf42fe711127
wharsojo <wharsojo@gmail.com>
2021-05-21 01:54:42 +0800

    feat: add autofill - try catch

--- ws-server/_autofill.js
+++ ws-server/_autofill.js
@@ -1,56 +1,76 @@
 const attach = require('../playwright/attach')
 const c = require('ansi-colors')
 
-async function _page(browser, url) {
-  let pages
-  let newpage
-  if (browser.pages) {
-    pages = await browser.pages()
-  } else {
-    pages = browser.contexts()[0].pages()
-  }
-  if (pages && pages.length > 1) {
-    const {origin} = new URL(url)
-    for (const pg of pages) {
-      if (pg.url().match(origin)) {
-        await pg.bringToFront()
-        newpage = pg
-        break
-      }
-    }  
-  }
-  if (!newpage) {
-    newpage = await browser.newPage()
-    attach(newpage)
-  } else if (newpage._page===undefined) {
-    console.log(c.redBright('Non automation page...'))
-    attach(newpage)
-  }
-  if (newpage.url()!==url) {
-    await newpage.goto(url)
-  }
-  return newpage
-}
-
-async function _save(page, oldPage, selector, store) {
-  const obj2 = await page.$eval(selector, (e, opt) => {
-    const [attr, key] = opt.split(':')
-    const [id1, id2] = attr.split('~')
-    const value = id2 ? e[id1](id2) : e[id1]
-    return {key, value}
-  }, store);
-  await oldPage.$eval('body', (e, obj2) => {
-    const {key, value: val} = obj2
-    localStorage.setItem(key, val)
-  }, obj2);
-}
-
 module.exports = async ({ data }) => {
   const c = require('ansi-colors')
   const { autofill, browser: b, _page: p, _frame: f } = data
   const browser = global.mitm.browsers[b]
   let page = await browser.currentTab(p, f)
   let oldPage = page
+
+  async function _page(url) {
+    let pages
+    let newpage
+    if (browser.pages) {
+      pages = await browser.pages()
+    } else {
+      pages = browser.contexts()[0].pages()
+    }
+    if (pages && pages.length > 1) {
+      const {origin} = new URL(url)
+      for (const pg of pages) {
+        if (pg.url().match(origin)) {
+          await pg.bringToFront()
+          newpage = pg
+          break
+        }
+      }  
+    }
+    if (!newpage) {
+      newpage = await browser.newPage()
+      attach(newpage)
+    } else if (newpage._page===undefined) {
+      console.log(c.redBright('Non automation page...'))
+      attach(newpage)
+    }
+    if (newpage.url()!==url) {
+      await newpage.goto(url)
+    }
+    page = newpage
+  }
+
+  async function _save(selector, store) {
+    const obj2 = await page.$eval(selector, (e, opt) => {
+      const [attr, key] = opt.split(':')
+      const [id1, id2] = attr.split('~')
+      const value = id2 ? e[id1](id2) : e[id1]
+      return {key, value}
+    }, store);
+    await oldPage.$eval('body', (e, obj2) => {
+      const {key, value: val} = obj2
+      localStorage.setItem(key, val)
+    }, obj2);
+  }
+
+  async function _leave() {
+    await oldPage.bringToFront()
+    page = oldPage
+  }
+
+  async function _close() {
+    await oldPage.bringToFront()
+    await page.close()
+    page = oldPage
+  }
+
+  async function _input(act, selector, value) {
+    if (value.match(/^:/)) {
+      value = await page.$eval('body', (e, key) => {
+        return localStorage.getItem(key)
+      }, value.slice(1));
+    }
+    page[act](selector, value)
+  }
 
   console.log(c.greenBright('>>> autofill'))
   let lastObj;
@@ -91,56 +111,35 @@ module.exports = async ({ data }) => {
      * input[name="email"] => :mailinator
      */
     const {
-      store = '',
       selector,
+      store='',
       value,
       act,
     } = obj
 
-    if (act) {
-      const [action, val] = act.split(':')
-      let options = {delay: val ? +val : 100}
-      if (action === 'type') {
-        await input('type', selector, store, options)
-      } else if (action === 'fill') {
-        await page.fill(selector, store, options)
-      } else if (action === 'wait') {
-        await page.waitForSelector(selector)
-      } else if (action === 'press') {
-        await page.press(selector, store)
-      } else if (action === 'click') {
-        await page.click(selector, store)
-      } else if (action === 'focus') {
-        await page.focus(selector)
-      } else if (action === 'check') {
-        await page.check(selector)
-      } else if (action === 'uncheck') {
-        await page.uncheck(selector)
-      } else if (action === 'selectOption') {
-        await page.selectOption(selector, store)
-      } else if (action === 'page') {
-        page = await _page(browser, store)
-      } else if (action === 'save') {
-        await _save(page, oldPage, selector, store)
-      } else if (action === 'leave') {
-        await oldPage.bringToFront()
-        page = oldPage
-      } else if (action === 'close') {
-        await page.close()
-        await oldPage.bringToFront()
-        page = oldPage
-      }
-    } else if (value) {
-      await input('fill', selector, value)
+    try {
+      if (act) {
+        const [action, val] = act.split(':')
+        let options = {delay: val ? +val : 100}
+        if (action === 'save'        ) { await _save(selector, store)              } else
+        if (action === 'type'        ) { await _input('type', selector, store)     } else
+        if (action === 'fill'        ) { await page.fill(selector, store, options) } else
+        if (action === 'selectOption') { await page.selectOption(selector, store)  } else
+        if (action === 'wait'        ) { await page.waitForSelector(selector)      } else
+        if (action === 'press'       ) { await page.press(selector, store)         } else
+        if (action === 'click'       ) { await page.click(selector, store)         } else
+        if (action === 'uncheck'     ) { await page.uncheck(selector)              } else
+        if (action === 'check'       ) { await page.check(selector)                } else
+        if (action === 'focus'       ) { await page.focus(selector)                } else
+        if (action === 'page'        ) { await _page(store)                        } else
+        if (action === 'close'       ) { await _close()                            } else
+        if (action === 'leave'       ) { await _leave()                            }
+      } else if (value) {
+        await _input('fill', selector, value)
+      }        
+    } catch (error) {
+      console.log(error)
     }
   }
   return { ok: 'OK' }
-  async function input(act, selector, value) {
-    if (value.match(/^:/)) {
-      value = await page.$eval('body', (e, key) => {
-        return localStorage.getItem(key)
-      }, value.slice(1));
-    }
-    page[act](selector, value)
-  }
 }
