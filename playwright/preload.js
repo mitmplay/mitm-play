45523a4f57536c284b1069ed3bb129130f726c72
wharsojo <wharsojo@gmail.com>
2021-07-04 03:47:47 +0800

    fix: unregister service worker

--- playwright/preload.js
+++ playwright/preload.js
@@ -1,20 +1,30 @@
 document.addEventListener('DOMContentLoaded', e => {
+  let retry = 1
+  let timeout = 400
+
   function unregisterServiceWorker(registrations) {
     for(let registration of registrations) {
       const {active} = registration
       if (active) {
         console.log('UNREGISTER:',active.scriptURL)
         registration.unregister()  
-      } else {
+      } else if (retry>7) {
         console.log('CANNOT UNREGISTER Service Worker!',active , registration)
+      } else {
+        console.log('UNREGISTER Retry:', retry)
+        setTimeout(fnTimeout, timeout)
+        retry += 1
+        break
       }
     }
   }
 
-  // console.log('UNREGISTER Service Worker')
-  setTimeout(async () => {
+  async function fnTimeout() {
     const {serviceWorker: w} = navigator
     const registrations = await w.getRegistrations()
     unregisterServiceWorker(registrations);
-  }, 800)
+  }
+
+  // console.log('UNREGISTER Service Worker')
+  setTimeout(fnTimeout, timeout)
 })
