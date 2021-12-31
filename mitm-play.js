0dc41e142d25a318b0b8f6183c4e0892538c956f
wharsojo <wharsojo@gmail.com>
2021-12-31 12:21:58 +0800

    feat: refactor mitm-play.js

--- mitm-play.js
+++ mitm-play.js
@@ -1,26 +1,23 @@
 const c = require('ansi-colors')
 const logmsg = require('./init/logmsg')
 
-const {NODE_OPTIONS: opt} = process.env
-const argv = process.argv.map(x=>x.replace(/\\/g, '/'))
-
 global.mitm = {fn: {logmsg}}
+let {env, argv} = process
 
-// process.argv.splice(1,0,'--max-http-header-size=80001')
 logmsg(c.red('\n[mitm-play.js]'))
-logmsg(c.yellow(`Argv as seen from NodeJS`), argv)
-if (opt) {
-  if (opt.match(/--max-http-header-size/)) {
+logmsg(c.yellow(`Argv as seen from NodeJS`), argv.map(x=>x.replace(/\\/g, '/')))
+
+if (env.NODE_OPTIONS) {
+  if (env.NODE_OPTIONS.match(/--max-http-header-size/)) {
     logmsg(c.red('Please check your NODE_OPTIONS: --max-http-header-size atleast 40960'))
-    logmsg(c.cyan(opt))
+    logmsg(c.cyan(env.NODE_OPTIONS))
   } else {
-    process.env.NODE_OPTIONS += ' --max-http-header-size=40960'
+    env.NODE_OPTIONS += ' --max-http-header-size=40960'
   }
 } else {
-  process.env.NODE_OPTIONS = '--max-http-header-size=40960'
+  env.NODE_OPTIONS = '--max-http-header-size=40960'
 }
-// process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
-logmsg(c.yellow(`NODE_OPTIONS=${process.env.NODE_OPTIONS}`))
+logmsg(c.yellow(`NODE_OPTIONS=${env.NODE_OPTIONS}`))
 
 global.__app = __dirname.replace(/\\/g, '/')
 logmsg(c.yellow(`App Path: ${global.__app}`))
