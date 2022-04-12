7fb976dbb319319cfb19d1a2ddbd41b3590e6e15
wharsojo <wharsojo@gmail.com>
2022-04-12 10:42:57 +0800

    fix: error parsing json

--- routes/jformat.js
+++ routes/jformat.js
@@ -3,8 +3,15 @@ const stringify = require("json-stringify-pretty-compact");
 function jformat(body, resp, __args) {
   if (__args.jformat) {
     if (resp.headers['content-type'].match(/json/)) {
-      let json = JSON.parse(`${body}`)
-      body = stringify(json, null, 2)
+      body = `${body}`
+      if (body.length) {
+        try {
+          let json = JSON.parse(body)
+          body = stringify(json, null, 2)          
+        } catch (error) {
+          console.error('JSON Err:', body)
+        }  
+      }
     }  
   }
   return body
